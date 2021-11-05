732237f1aa42f87e451982671759b5c7aa00eea3
kimjunho <gsgs2007@naver.com>
2021-11-05 21:48:29 +0900

    First Commit

---
+++ List/List.cpp
@@ -0,0 +1,23 @@
+﻿#include <forward_list>
+#include "DoubleLinkedList.h"
+#include <iostream>​
+#include <list>
+#include "SingleLinkedList.h"
+
+using namespace std;
+
+
+int main()
+{
+	list<int> list;
+
+	list.push_back(4);
+	list.push_back(5);
+	list.push_front(2);
+
+	for (auto iter = list.begin(); iter != list.end(); ++iter)
+	{
+		cout << *iter << " ";
+	}
+	cout << endl;
+}
